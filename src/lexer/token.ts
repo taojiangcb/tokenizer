b257c49dc87c86085d9902462fb296eac501f108
jiangtao <jiangtao_qepyou@163.com>
2021-08-10 22:54:44 +0800

    first commit

---
+++ src/lexer/token.ts
@@ -0,0 +1,28 @@
+import TokenType from './tokenTye';
+export default class Token {
+  private type: TokenType;
+  private value: string;
+  constructor(type: TokenType, value: string) {
+    this.type = type;
+    this.value = value;
+  }
+
+  getType(): TokenType {
+    return this.type;
+  }
+
+  isVariable(): boolean {
+    return this.type === TokenType.VARIABLE;
+  }
+
+  isScalar(): boolean {
+    return this.type === TokenType.FLOAT
+      || this.type === TokenType.INTEGER
+      || this.type === TokenType.BOOLEAN
+      || this.type === TokenType.STRING
+  }
+
+  toString() {
+    return `type:${this.type},value:${this.value}`;
+  }
+}
---
+++ src/lexer/tokenTye.ts
@@ -0,0 +1,12 @@
+enum TokenType {
+  KEYWORD = 1,
+  VARIABLE = 2,
+  OPERATOR = 3,
+  BREACKET = 4,
+  INTEGER = 5,
+  FLOAT = 6,
+  BOOLEAN = 7,
+  STRING = 8
+}
+
+export default TokenType
